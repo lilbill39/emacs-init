61134cb8cf3cde92ba114ce7e79892cc3b1d17d5
Ryan Livingston <lilbill39@users.noreply.github.com>
2020-01-30 09:05:48 -0500

    fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,5 @@
 #set -g theme_color_scheme terminal-dark-white
 set -x EDITOR vim
 set -x FZF_DEFAULT_COMMAND fd
-set -x FZF_ALT_C_COMMAND fd -t d
+set -x FZF_ALT_C_COMMAND fd -t d --no-ignore --exclude node_modules 
 set -x FZF_CTRL_T_COMMAND fd
